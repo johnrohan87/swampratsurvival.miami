4a0fbda8101bcbb82addc875a207a1ca09694376
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2021-02-27 07:19:05 -0500

    removed GlobalContext moving to Redux

--- packages/landing-gatsby/src/pages/index.js
+++ packages/landing-gatsby/src/pages/index.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, Fragment, useContext } from 'react';
-import { graphql, useStaticQuery, Link } from "gatsby";
-import PropTypes from "prop-types"
+import React, { useState, useEffect, Fragment } from 'react';
+import { Link } from "gatsby";
 import { ThemeProvider } from 'styled-components';
 import Sticky from 'react-stickynode';
 import { Modal } from '@redq/reuse-modal';
@@ -15,7 +14,6 @@ import Navbar from 'containers/App/Navbar';
 import DomainSection from 'containers/App/SRSBanner';
 
 import { DrawerProvider } from 'common/contexts/DrawerContext';
-import { GlobalContextProvider } from 'common/contexts/GlobalContext';
 import '@redq/reuse-modal/es/index.css';
 import SEO from 'components/seo';
 
@@ -49,10 +47,6 @@ function useWindowSize() {
 export default ({data}) => {  
   const size = process.browser && useWindowSize();
   const innerWidth = process.browser && size.innerWidth;
-  //const dispatch = useContext(GlobalDispatchContext);
-  //const state = useContext(GlobalStateContext);
-  //console.log(dispatch);
-  //console.log(state);
 
   return (
     <ThemeProvider theme={appTheme}>
@@ -67,12 +61,10 @@ export default ({data}) => {
               <Navbar />
             </DrawerProvider>
           </Sticky>
-          <GlobalContextProvider>
-            <h1>Hello from Swamp Rat Survival</h1>
-            <h2>{/*state.user ? "Loged in as " + state.user : "Not Loged In"*/}</h2>
-            <Link to="/app" >Go to App Page</Link>
-            <DomainSection />
-          </GlobalContextProvider>
+          <h1>Hello from Swamp Rat Survival</h1>
+          <h2>{/*state.user ? "Loged in as " + state.user : "Not Loged In"*/}</h2>
+          <Link to="/app" >Go to App Page</Link>
+          <DomainSection />
         </AppWrapper>
       </Fragment>
     </ThemeProvider>
