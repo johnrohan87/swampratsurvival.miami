50a4d22ec16262970a2f4afcd117c62daea4b4ec
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2021-02-28 12:25:51 -0500

    working on store - not working

--- packages/landing-gatsby/src/pages/index.js
+++ packages/landing-gatsby/src/pages/index.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, Fragment } from 'react';
-//import ReactDOM from "react-dom";
+
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
-
 import reducers from "../reducers/index";
 
 import { Link } from "gatsby";
@@ -19,7 +19,7 @@ import {
 import { ResetCSS } from 'common/assets/css/style';
 import Navbar from 'containers/App/Navbar';
 import DomainSection from 'containers/App/SRSBanner';
-//import PostListContainer from '../common/components/postList';
+import PostListContainer from '../common/components/postList';
 
 import { DrawerProvider } from 'common/contexts/DrawerContext';
 import '@redq/reuse-modal/es/index.css';
@@ -74,6 +74,8 @@ export default ({data}) => {
           <Provider store={store}>
             <h1>Hello from Swamp Rat Survival</h1>
             <h2>{/*state.user ? "Loged in as " + state.user : "Not Loged In"*/}</h2>
+            <h2>Store: {store ? "Store = " + store : "No Store"}</h2>
+            <PostListContainer />
             <Link to="/app" >Go to App Page</Link>
             <DomainSection />
           </Provider>
